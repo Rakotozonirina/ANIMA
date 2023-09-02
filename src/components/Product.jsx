c486fe8ea0165e7dfcc1b824e7801ab221c3e236
Zo Nirina <tomzonirina@gmail.com>
2023-09-02 15:48:53 +0300

    feat: the page in the half

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import IlustrationFlower from '../public/illustration_flower.png'
 import IlustrationFlowerOne from '../public/illustration_flower_one.png'
+import IlustrationFlowerTwo from '../public/illustration_flower_two.png'
+import IlustrationFlowerThree from '../public/illustration_flower_three.png'
+import IlustrationFlowerFour from '../public/illustration_flower_four.png'
+import IlustrationFlowerFive from '../public/illustration_flower_five.png'
+import IlustrationFlowerSix from '../public/illustration_flower_six.png'
+import IlustrationFlowerSeven from '../public/illustration_flower_seven.png'
+import IlustrationFlowerHeight from '../public/illustration_flower_height.png'
 import Food from '../public/food.png'
 import FoodOne from '../public/food_one.png'
 import FoodTwo from '../public/food_two.png'
 import FoodThree from '../public/food_three.png'
+import HeroFood from '../public/hero_food.png'
 
 const Datas = [
     {
@@ -39,7 +47,7 @@ const Datas = [
 
 const Product = () => {
     return(
-        <main className='bg-gray-900/25 w-full mx-auto px-[60px] h-[200vh]'>
+        <main className='bg-gray-900/25 w-full flex flex-col gap-[200px] mx-auto px-[60px] h-[260vh]'>
             <section className='flex flex-col gap-[150px]'>
                 <section className='flex items-center justify-center gap-4'>
                     <div>
@@ -53,7 +61,7 @@ const Product = () => {
                         <img src={IlustrationFlowerOne} alt="deuxième illustration flower" width={180} height={253} />
                     </div>
                 </section>
-                <section className='relative flex items-center justify-center gap-4'>
+                <section className='relative flex items-center justify-center gap-6'>
                     {Datas.map((Data) => (
                         <section className='w-[265px] h-[309px] border border-white flex items-end justify-center relative shadow-lg rounded-tl-[12px] rounded-br-[12px]' key={Data.id}>
                             <section className='absolute top-[-30%] left-[10%]'>
@@ -68,10 +76,41 @@ const Product = () => {
                             </article>
                         </section>
                     )) }
-                    <section></section>
-                    <section></section>
-                    <section></section>
+                    <section className='absolute bottom-[-10%] left-[20%]'>
+                        <img src={IlustrationFlowerTwo} alt="deuxième illustration" />
+                    </section>
+                    <section className='absolute bottom-[-60%] left-[40%]'>
+                        <img src={IlustrationFlowerFive} alt="quatrième illustration" />
+                    </section>
+                    <section className='absolute bottom-[-10%] right-[15%]'>
+                        <img src={IlustrationFlowerThree} alt="troisème illustration" />
+                    </section>
+                    <section className='absolute bottom-[-10%] right-0'>
+                        <img src={IlustrationFlowerFour} alt="quatrième illustration" />
+                    </section>
                 </section>
+            </section>
+            <section className='flex'>
+                <div className='grow'>
+                    <img src={HeroFood} alt="hero food" width={600} />
+                </div>
+                <article className='flex grow flex-col justify-center relative gap-4'>
+                        <section>
+                            <img src={IlustrationFlower} alt="premier flower" className='absolute top-0 left-[-10%]' />
+                        </section>
+                        <h1 className='w-[442px] text-gray-900 text-6xl font-bold leading-[65px]'>Bienvenue dans notre restaurant</h1>
+                        <p className='w-[376px] text-gray-500 text-start text-base font-medium leading-[29px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <button className='w-[448px] p-3 bg-gray-900 text-center text-white text-xl font-bold leading-[29px] shadow-lg rounded-tl-[12px] rounded-br-[12px]'>Liste des produits</button>
+                        <section className='absolute bottom-0 left-[-30%]'>
+                            <img src={IlustrationFlowerSix} alt="six flower" />
+                        </section>
+                        <section className='absolute bottom-[-10%] left-0'>
+                            <img src={IlustrationFlowerSeven} alt="seven flower"/>
+                        </section>
+                        <section className='absolute bottom-0 right-[-10%]'>
+                            <img src={IlustrationFlowerHeight} alt="height flower"/>
+                        </section>
+                </article>
             </section>
         </main>
     )
