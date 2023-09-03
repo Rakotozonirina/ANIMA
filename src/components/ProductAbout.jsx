8fda6eb4459cfa4fde49a1d08d71e34d4a80acba
Zo Nirina <tomzonirina@gmail.com>
2023-09-03 21:46:14 +0300

    feat: finish of the second page but not responsive

--- src/components/ProductAbout.jsx
+++ src/components/ProductAbout.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import AboutOne from '../public/about_one.png'
+import AboutTwo from '../public/about_two.png'
+import Slice from '../public/slice.png'
+import SliceTwo from '../public/slice_bake.png'
+import SliceOne from '../public/slice_one.png'
+import IlustrationFlower from '../public/illustration_flower.png'
+import IlustrationFlowerFour from '../public/illustration_flower_four.png'
+import IlustrationFlowerThree from '../public/illustration_flower_three.png'
+import IlustrationFlowerOne from '../public/illustration_flower_one.png'
+import Rosemary from '../public/rosemary.png'
+
 
 const ProductAbout = () => {
 return (
@@ -18,6 +28,71 @@ return (
                     <p className='text-justify text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.</p>
                 </article>
             </section>
+        </section>
+        <section className='flex items-center justify-center gap-4'>
+            <div>
+                <img src={IlustrationFlower} alt="premier illustration flower" width={224} height={212} />
+            </div>
+            <article className='flex flex-col items-center justify-center'>
+                <h1 className='text-center text-gray-900 text-6xl font-bold leading-[65px]'>Présentation</h1>
+                <p className='w-[376px] text-center text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui nostrum Nulla qui nostrum</p>
+            </article>
+            <div>
+                <img src={IlustrationFlowerOne} alt="deuxième illustration flower" width={180} height={253} />
+            </div>
+        </section>
+        <section>
+            <img src={AboutTwo} alt="about two" />
+        </section>
+        <section className='h-[40vh] flex items-center justify-center'>
+            <article>
+                <h2 className='text-center text-black text-5xl font-bold leading-[64px]'>Notre Marque</h2>
+                <p className='w-[452px] text-center text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </article>
+        </section>
+        <section className='relative flex flex-col gap-[80px] h-[280vh]'>
+            <section className='flex gap-4'>
+                <div className='grow'>
+                    <img src={Slice} alt="slice" />
+                </div>
+                <article className='grow w-[568px] flex flex-col justify-center'>
+                    <h2 className='text-black text-[88px] font-bold leading-[105.60px]'>01.Slice</h2>
+                    <p className='text-justify text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. Convallis pellentesque quis fringilla sagittis. Egestas in risus sit nunc nunc, arcu donec nam etiam.</p>
+                </article>
+            </section>
+            <div className='absolute top-[26%] left-[-10%]'>
+                <img src={Rosemary} alt="rosemary" />
+            </div>
+            <section className='flex justify-between h-[180vh]'>
+                <section className='flex items-end'>
+                    <div className='w-[512px] h-[719px]'>
+                        <article>
+                            <h2 className='text-black text-[88px] font-bold leading-[105.60px]'>03.Bake</h2>
+                            <p className='text-justify mb-8 text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. Convallis pellentesque quis fringilla sagittis. Egestas in risus sit nunc nunc, arcu donec nam etiam.</p>
+                        </article>
+                        <section>
+                            <img src={SliceTwo} alt="bake pic" />
+                        </section>
+                    </div>
+                </section>
+                <section>
+                    <div className='w-[512px] h-[719px]'>
+                        <section>
+                            <img src={SliceOne} alt="second slice" />
+                        </section>
+                        <article>
+                            <h2 className='text-black text-[88px] font-bold leading-[105.60px]'>02.Pickled</h2>
+                            <p className='text-justify text-neutral-600 text-2xl font-normal leading-[33.60px] tracking-wide'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. Convallis pellentesque quis fringilla sagittis. Egestas in risus sit nunc nunc, arcu donec nam etiam.</p>
+                        </article>
+                    </div>
+                </section>
+            </section>
+            <div className='absolute bottom-[15%] right-[20%]'>
+                <img src={IlustrationFlowerThree} alt="flower three" />
+            </div>
+            <div className='absolute bottom-[15%] right-0'>
+                <img src={IlustrationFlowerFour} alt="flower four" />
+            </div>
         </section>
     </main>
 )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavbarAbout from '../components/NavbarAbout'
 import ProductAbout from '../components/ProductAbout'
+import Footer from '../components/Footer'
 
 const About = () => {
 return (
     <div>
         <NavbarAbout/>
         <ProductAbout/>
+        <Footer/>
     </div>
 )
 }
