452d07f34f3e0f3087f7b29f7af205d39a1d3c77
Zo Nirina <tomzonirina@gmail.com>
2023-09-06 21:58:00 +0300

    feat: Adding something

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,21 @@ import { RiTwitterLine } from "react-icons/ri";
 
 const Navbar = () => {
 return (
-    <header className="container mx-auto px-[60px] h-[120vh] relative bg-white">
-        <nav className='py-4 flex items-center justify-between'>
+    <header className="container mx-auto px-[60px] h-[200vh] lg:h-[120vh] relative bg-white">
+        <nav className='py-8 flex flex-wrap justify-center gap-4 items-center lg:gap-0 lg:flex-nowrap lg:py-4 lg:flex lg:items-center lg:justify-between'>
             <img src={Logo} alt='Logo' width={120} height={120} />
             <h2 className='text-orange-500 cursor-default text-xl font-black underline uppercase leading-[29px]'>acceuil</h2>
             <Link to="/contact" className='p-3 shadow-lg rounded-tl-lg rounded-br-lg bg-orange-500 text-white text-base font-bold leading-[29px]'>
                 Contactez-nous
             </Link>
         </nav>
-        <section className='absolute top-[-30%] left-[-10%]'>
+        <section className='lg:absolute scale-[0.5] lg:scale-[auto] absolute top-[-40%] left-[-60%] lg:top-[-30%] lg:left-[-10%]'>
             <img src={OtherImage} alt="circles"/>
         </section>
-        <section className='h-[78vh] mt-8 flex'>
-            <article className='w-[580px] flex flex-col gap-2 justify-center grow'>
-                <h1 className='text-gray-900 text-[54px] font-bold leading-[60px]'>Nous fournissons la meilleure nourriture pour vous</h1>
-                <p className='text-gray-500 text-base font-medium leading-[29px] w-[376px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eveniet mollitia perferendis odit cum iusto ullam consectetur.</p>
+        <section className='h-[140vh] lg:h-[78vh] mt-8 lg:mt-8 lg:flex'>
+            <article className='lg:w-[580px] lg:flex lg:flex-col lg:gap-2 lg:justify-center grow'>
+                <h1 className='text-gray-900 text-[42px] lg:text-[54px] font-bold leading-[60px]'>Nous fournissons la meilleure nourriture pour vous</h1>
+                <p className='text-gray-500 text-base font-medium leading-[29px] md:w-[376px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos eveniet mollitia perferendis odit cum iusto ullam consectetur.</p>
                 <div className='w-max my-3'>
                     <Link to="/about">
                         <button className='capitalize w-[120px] p-3 shadow-lg rounded-tl-lg rounded-br-lg bg-[#101A24] text-white text-base font-bold leading-[29px]'>à propos</button>
@@ -47,8 +47,8 @@ return (
                     <span className='w-[143px] h-[0px] opacity-20 border border-black'></span>
                 </div>
             </article>
-            <section className='w-[580px] relative grow'>
-                <img src={ImageHero} alt="hero page" className='absolute top-[-15%] left-[-15%]' />
+            <section className='lg:w-[580px] relative grow'>
+                <img src={ImageHero} alt="hero page" className='lg:absolute md:top-[-15%] lg:left-[-15%]' />
             </section>
         </section>
     </header>
