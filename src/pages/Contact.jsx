e8bcf9c4d6ca68e20c63fa295c95a7611dba8ae9
Zo Nirina <tomzonirina@gmail.com>
2023-09-09 15:58:55 +0300

    feat: finish of the responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
 return (
     <div className='bg-no-repeat flex items-center justify-center bg-bottom bg-cover object-contain' style={{backgroundImage: `url(${BgContact})`, height: '100vh'}}>
         <section className='w-[90%] h-[94vh] flex relative bg-white rounded-[50px] shadow'>
-            <section className='w-[70%]'>
+            <section className='w-[90%] md:w-[70%] md:scale-[0.8] lg:scale-[0.9] xl:scale-[1]'>
                 <div>
                     <section className='py-2.5 px-16'>
                         <Link to="/">
@@ -18,22 +18,22 @@ return (
                     </section>
                     <article className='px-16'>
                         <h2 className='text-black text-[32px] font-bold'>Restaurant <span className="text-orange-500 text-[32px] font-bold">le QG</span></h2>
-                        <p className='text-black w-[60%] my-1 text-sm font-semibold leading-normal tracking-tight'>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
+                        <p className='text-black w-full md:w-[60%] my-1 text-sm font-semibold leading-normal tracking-tight'>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
                     </article>
                 </div>
-                <section className='px-16 h-[48vh]'>
-                    <form action="" className='flex flex-col justify-center gap-4 w-[60%] h-[48vh]'>
+                <section className='px-16 h-[30vh] md:h-[48vh]'>
+                    <form action="" className='flex flex-col justify-center gap-4 w-full md:w-[80%] lg:w-[60%] h-[48vh]'>
                         <input type="text" name="name" placeholder='Nom*'/>
                         <input type="email" name="email" placeholder='Email' />
                         <input type="tel" name="phone" placeholder='Numéro de téléphone*' />
                         <textarea name="message" cols="30" rows="2" className='border border-neutral-200 text-zinc-500 text-sm font-medium leading-normal tracking-tight p-2.5' placeholder='Message...' ></textarea>
-                        <button type='submit' className='border border-red-400 py-2.5 bg-orange-500 rounded justify-center items-center inline-flex text-white text-base font-semibold uppercase'>envoyer</button>
+                        <button type='submit' className='py-2.5 bg-orange-500 rounded justify-center items-center inline-flex text-white text-base font-semibold uppercase'>envoyer</button>
                     </form>
                 </section>
-                <section className='px-16 h-[10vh]'>  
-                    <section className='w-[60%] grid grid-cols-3 h-[10vh]'>
-                        <div className='flex items-center gap-4'>
-                            <span>
+                <section className='px-16 pt-[6rem] md:pt-0 md:h-[10vh]'>  
+                    <section className='w-full md:w-[80%] lg:w-[60%] grid grid-cols-3 md:h-[10vh]'>
+                        <div className='flex items-center gap-4 scale-[0.7] md:scale-[0.8] lg:scale-[0.9] xl:scale-[1]'>
+                            <span >
                                 <BsTelephone/>
                             </span>
                             <article> 
@@ -41,7 +41,7 @@ return (
                                 <p className='text-orange-500 font-normal leading-tight tracking-tight'>03 5432 1234</p>
                             </article>
                         </div>
-                        <div className='flex items-center gap-4'>
+                        <div className='flex items-center gap-4 scale-[0.7] md:scale-[0.8] lg:scale-[0.9] xl:scale-[1]'>
                             <span>
                                 <BsReverseLayoutTextSidebarReverse/>
                             </span>
@@ -50,7 +50,7 @@ return (
                                 <p className='text-orange-500 font-normal leading-tight tracking-tight'>03 5432 1234</p>
                             </article>
                         </div>
-                        <div className='flex items-center gap-4'>
+                        <div className='flex items-center gap-4 scale-[0.7] md:scale-[0.8] lg:scale-[0.9] xl:scale-[1]'>
                         <span>
                             <BsEnvelopePaper/>
                         </span>
@@ -62,8 +62,8 @@ return (
                     </section>
                 </section>
             </section>
-            <section className='w-[30%] rounded-r-[50px] bg-black'></section>
-            <section className='absolute top-[10%] right-[10%]'>
+            <section className='w-[10%] md:w-[30%] rounded-r-[50px] bg-black'></section>
+            <section className='absolute top-[10%] right-[10%] hidden lg:block md:scale-[0.8] lg:scale-[0.9] xl:scale-[1]'>
                 <img src={HeroImage} alt="hero contact" width={500} height={700} />
             </section>
         </section>
